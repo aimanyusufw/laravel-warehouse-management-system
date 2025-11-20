84c256388e68bb40704c3a1bbb20d0ea2748f362
aimanyusufw <aimanyusufdev@gmail.com>
2025-11-20 21:40:30 +0700

    add : add function to auto make a qr code

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Product extends Model
 {
@@ -26,6 +27,24 @@ class Product extends Model
         'retail_price' => 'decimal:2',
         'weight' => 'decimal:3',
     ];
+
+    public function generateAndSaveQrCode(): void
+    {
+        if (!Storage::disk('public')->exists('qrcodes/products')) {
+            Storage::disk('public')->makeDirectory('qrcodes/products');
+        }
+
+        $qrCodeData = (string) $this->sku;
+        $fileName = 'location_' . $this->sku . '.svg';
+        $path = 'qrcodes/products/' . $fileName;
+
+        \SimpleSoftwareIO\QrCode\Facades\QrCode::size(200)
+            ->format('svg')
+            ->generate($qrCodeData, storage_path('app/public/' . $path));
+
+        $this->qr_code_path = $path;
+        $this->saveQuietly();
+    }
 
     public function inventories()
     {
