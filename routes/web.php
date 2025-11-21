9ed5c1b8a604bc44bd317f0ea2660485033f58eb
aimanyusufw <aimanyusufdev@gmail.com>
2025-11-21 22:08:11 +0700

    ref : remove download location qr route

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,3 @@ Route::get('locations/print-qr/{location}', function (Location $location) {
     ]);
     return $pdf->stream('label.pdf');
 })->name("location.print");
-
-Route::get('locations/download-qr/{location}', function (Location $location) {
-    $pdf = Pdf::loadView('filament.print_qr', ["location" => $location]);
-    return $pdf->download("lcoation label-" . $location->id . '.pdf');
-})->name("location.download");
